28b118087ee112a4a47d2391e873e06ee66b6953
TomC <TomC@users.noreply.github.com>
2020-09-26 17:06:49 +0100

    Update Configuration.h
    move pid values to new location

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,9 +493,11 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.0 }
     #define DEFAULT_Kd_LIST { 114.00, 112.0 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+  // Creality Ender-3 - M303 E0 S200 C8 U1
+  #define DEFAULT_Kp 21.82
+  #define DEFAULT_Ki 1.96
+  #define DEFAULT_Kd 60.77
+
   #endif
 #endif // PIDTEMP
 
